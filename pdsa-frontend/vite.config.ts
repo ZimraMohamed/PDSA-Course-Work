08959307dd090c437c345dcc9c5b74eeac5acec1
ZimraMohamed <zimramohamed64@gmail.com>
2026-01-29 16:00:22 +0530

    updated

--- pdsa-frontend/vite.config.ts
+++ pdsa-frontend/vite.config.ts
@@ -1,11 +1,28 @@
+// import { defineConfig } from 'vitest/config'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     port: 3000, // Keep same port as your old CRA setup
+//   },
+//   test: {
+//     globals: true,
+//     environment: 'jsdom',
+//     setupFiles: ['./src/setupTests.ts'],
+//   },
+// })
+
 import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // <--- ADD THIS LINE FOR PRODUCTION
   server: {
-    port: 3000, // Keep same port as your old CRA setup
+    port: 3000, 
   },
   test: {
     globals: true,
